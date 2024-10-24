39268b1b6967bafa713932329547b5b7e25372c2
Muhammad Ahsan <m.ahsan9981@gmail.com>
2024-10-24 08:13:14 +0500

    intial setup (continued...)

--- server.js
+++ server.js
@@ -4,5 +4,30 @@ const morgan = require('morgan');
 const helmet = require('helmet');
 const config = require('config');
 
+console.log(config.get('db.connection'))
+
+mongoose.connect(config.get('db.connection'))
+.then(() => console.log('Connected to TradeMatePro mongodb database'))
+.catch((error) => console.error('Error connecting to TradeMatePro database....', error));
 
 const server = express();
+
+
+
+server.set('view engine', 'ejs');
+server.set('views', './views');
+
+server.use(helmet());
+server.use(express.json());
+server.use(express.urlencoded({extended: true}));
+server.use(express.static('public'));
+
+if (server.get('env') === 'development') {
+    server.use(morgan('dev'));
+}
+
+
+
+const port = config.get('server.port');
+
+server.listen(port, () => console.log(`TradeMatePro server listening at port ${port}`));
